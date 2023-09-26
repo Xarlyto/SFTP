55b1bcb7fe5e6548161e30a3ad4e3588b7f9d022
xarly <xarly@OMEN-XLTO>
2023-09-26 21:31:59 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string host = "eu-central-1.sftpcloud.io";
+        string username = "cb04906e609b4004a36013854650a1a6";
+        string password = "G0vwx7H4WjNSvpmcG04onCP2MJllmCvK";
+
+        using (var sftpManager = new SftpManager(host, username, password))
+        {
+            try
+            {
+                Console.WriteLine(AppContext.BaseDirectory);
+
+                // Upload a file
+                sftpManager.UploadFile(AppContext.BaseDirectory + "/files/upload/localfile.txt", "remotefile.txt");
+
+                // Download a file
+                sftpManager.DownloadFile("remotefile_2.txt", AppContext.BaseDirectory + "/files/download/downloadedfile.txt");
+
+                Console.WriteLine("File operations completed successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ src/SftpClient.cs
@@ -0,0 +1,39 @@
+﻿using Renci.SshNet;
+using System;
+using System.IO;
+
+public class SftpManager : IDisposable
+{
+    private SftpClient sftpClient;
+
+    public SftpManager(string host, string username, string password)
+    {
+        sftpClient = new SftpClient(host, username, password);
+        sftpClient.Connect();
+    }
+
+    public void UploadFile(string localFilePath, string remoteFilePath)
+    {
+        using (var fileStream = new FileStream(localFilePath, FileMode.Open))
+        {
+            sftpClient.UploadFile(fileStream, remoteFilePath);
+        }
+    }
+
+    public void DownloadFile(string remoteFilePath, string localFilePath)
+    {
+        using (var fileStream = new FileStream(localFilePath, FileMode.Create))
+        {
+            sftpClient.DownloadFile(remoteFilePath, fileStream);
+        }
+    }
+
+    public void Dispose()
+    {
+        if (sftpClient != null && sftpClient.IsConnected)
+        {
+            sftpClient.Disconnect();
+            sftpClient.Dispose();
+        }
+    }
+}
